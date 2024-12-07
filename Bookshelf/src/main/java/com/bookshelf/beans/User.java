ccab255d26f7e86bf9ab5ca9af736cba7c87aac2
RachelMonero <rachel.monero@gmail.com>
2024-12-06 22:09:31 -0500

    Modified all Java files in the beans package,Created the library builder

--- Bookshelf/src/main/java/com/bookshelf/beans/User.java
+++ Bookshelf/src/main/java/com/bookshelf/beans/User.java
@@ -1,26 +1,38 @@
 package com.bookshelf.beans;
 
 public class User {
+	private String user_id;
     private String email;
     private String username;
     private String password;
     private String first_name;
     private String last_name;
-
-    // User input for Address instance
-    private String address;
-    private String city;
-    private String province;
-    private String country;
-    private String postal_code;
-    
+    private String address_id;
     private boolean is_verified;
     
-    public User(String first_name, String last_name, String password, boolean isVerified) {
+    public User(String first_name, String last_name, String password, boolean is_verified) {
     	this.setFirst_name(first_name);
     	this.setLast_name(last_name);
     	this.setPassword(password);
     	this.setIs_verified(is_verified);
+    }
+    
+    public User(String user_id, String username, String email, String password, String first_name, String last_name, String address_id, boolean is_verified) {
+    this.user_id = user_id;
+    this.username = username;
+    this.email = email;
+    this.password = password;
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.address_id = address_id;
+    this.is_verified = is_verified;
+}
+    
+    public String getUser_id() {
+    	return user_id;
+    }
+    public void setUser_id(String user_id) {
+    	this.user_id = user_id;
     }
 
 	public String getEmail() {
@@ -62,47 +74,12 @@ public class User {
 	public void setLast_name(String last_name) {
 		this.last_name = last_name;
 	}
-
-	public String getAddress() {
-		return address;
+	public String getAddress_id() {
+		return address_id;
 	}
-
-	public void setAddress(String address) {
-		this.address = address;
+	public void setAddress_id(String address_id) {
+		this.address_id = address_id;
 	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public String getProvince() {
-		return province;
-	}
-
-	public void setProvince(String province) {
-		this.province = province;
-	}
-
-	public String getCountry() {
-		return country;
-	}
-
-	public void setCountry(String country) {
-		this.country = country;
-	}
-
-	public String getPostal_code() {
-		return postal_code;
-	}
-
-	public void setPostal_code(String postal_code) {
-		this.postal_code = postal_code;
-	}
-
 	public boolean getIs_verified() {
 		return is_verified;
 	}
@@ -110,4 +87,6 @@ public class User {
 	public void setIs_verified(boolean is_verified) {
 		this.is_verified = is_verified;
 	}
+	
+
 }
