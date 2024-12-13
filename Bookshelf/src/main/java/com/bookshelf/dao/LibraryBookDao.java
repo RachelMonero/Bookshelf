aef0657e3d9cc6cc06e7f6967af17e5ff6995ef2
Nadia JujoosteE <yourgitemail@example.com>
2024-12-12 21:31:02 -0500

    Librarians can view/edit books associated to their location

--- Bookshelf/src/main/java/com/bookshelf/dao/LibraryBookDao.java
+++ Bookshelf/src/main/java/com/bookshelf/dao/LibraryBookDao.java
@@ -5,11 +5,16 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.bookshelf.beans.Book;
 import com.bookshelf.beans.LibraryBook;
 import com.bookshelf.beans.User;
 import com.bookshelf.connection.DBConnection;
 import com.bookshelf.connection.DBUtil;
+import com.bookshelf.libs.UUIDGenerator;
+
 
 public class LibraryBookDao {
 
@@ -99,4 +104,68 @@ public class LibraryBookDao {
             e.printStackTrace();
         }
     }
+    
+    public static List<Book> getBooksByLibraryId(String libraryId) {
+        List<Book> books = new ArrayList<>();
+        String query = "SELECT b.book_id, b.title, b.author, b.isbn, b.published_year, b.genre_id " +
+                       "FROM bookshelf_library_book lb " +
+                       "JOIN bookshelf_book b ON lb.book_id = b.book_id " +
+                       "WHERE lb.library_id = ?";
+        try (Connection connection = DBConnection.getDBInstance();
+             PreparedStatement preparedStmt = connection.prepareStatement(query)) {
+            preparedStmt.setString(1, libraryId);
+            ResultSet resultSet = preparedStmt.executeQuery();
+            while (resultSet.next()) {
+                books.add(new Book(
+                    resultSet.getString("book_id"),
+                    resultSet.getString("title"),
+                    resultSet.getString("author"),
+                    resultSet.getString("isbn"),
+                    resultSet.getInt("published_year"),
+                    resultSet.getString("genre_id") // Updated to fetch the correct column
+                ));
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return books;
+    }
+
+
+    public static int countBooksInLibrary(String book_id, String libraryId) {
+        int count = 0;
+        String query = "SELECT COUNT(*) AS book_count FROM bookshelf_library_book " +
+                       "WHERE book_id = ? AND library_id = ?";
+        try (Connection connection = DBConnection.getDBInstance();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, book_id);
+            statement.setString(2, libraryId);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                count = resultSet.getInt("book_count");
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return count;
+    }
+
+    public static boolean linkBookToLibrary(String bookId, String libraryId) {
+        String query = "INSERT INTO bookshelf_library_book (library_book_id, library_id, book_id, added_date, is_available) VALUES (?, ?, ?, NOW(), true)";
+        try (Connection connection = DBConnection.getDBInstance();
+             PreparedStatement preparedStmt = connection.prepareStatement(query)) {
+            // Generate a unique ID for library_book_id
+            String libraryBookId = UUIDGenerator.generateUUID();
+            preparedStmt.setString(1, libraryBookId); // Set library_book_id
+            preparedStmt.setString(2, libraryId);     // Set library_id
+            preparedStmt.setString(3, bookId);        // Set book_id
+            int rowsInserted = preparedStmt.executeUpdate();
+            return rowsInserted > 0;
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+
 }
